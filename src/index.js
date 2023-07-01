ae8168176ebafb1615321b39c365256ce95086fc
Diebut <guevaradiego_2011@hotmail.com>
2023-07-01 20:32:27 -0300

    se agrega en el middlewares codigo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+//middlewares
+app.use(express.json());// se encarga de una app cliente envie el formato json
+app.use(express.urlencoded({extended: false})); //permite esta funcion cuando envie un dato en formulario es capaz procesarlo y enviarlo en un object y false para aceptar datos simples no imagenes
+
 //routes
 app.use(require('./routes/index'));
 
