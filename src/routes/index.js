ebf228dd927cd0003dc01ba94ad39cef90115b32
Diebut <guevaradiego_2011@hotmail.com>
2023-07-01 21:07:16 -0300

    creamos una const getUsers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/users', );
+const { getUsers } = require('../controllers/index.controller');
+router.get('/users', getUsers);
 
 module.exports = router;
