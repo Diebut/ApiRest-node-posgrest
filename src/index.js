01f6ba721d3a205a842a4ac75b628e10a2c0f084
Diebut <guevaradiego_2011@hotmail.com>
2023-07-07 18:26:48 -0300

    se hacen modificaiones

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = process.env.PORT || 4000; //se agrego
 
 //middlewares
-app.use(express.json());// se encarga de una app cliente envie el formato json
-app.use(express.urlencoded({extended: false})); //permite esta funcion cuando envie un dato en formulario es capaz procesarlo y enviarlo en un object y false para aceptar datos simples no imagenes
+//app.use(express.json());// se encarga de una app cliente envie el formato json
+//app.use(express.urlencoded({extended: false})); //permite esta funcion cuando envie un dato en formulario es capaz procesarlo y enviarlo en un object y false para aceptar datos simples no imagenes
+
+app.set('json spaces', 4); // es para q se vea el json en forma vertical ordenado en la pagina web
+app.use(bodyParser.urlencoded({extended: false}));  //nuevo se agrego
+app.use(bodyParser.json()); //nuevo se agrego
+app.use(cors()); //nuevo se agrego
 
 //routes
 app.use(require('./routes/index'));
